8ddee9c391c2c1d1caa12352b5833a4f99811209
KarlaRosas19 <49324882+KarlaRosas19@users.noreply.github.com>
2020-10-27 14:05:12 +0100

    First test with a URL: https://en.wikipedia.org/wiki/Comparison_(grammar)
    Results:
    -5 CSV
    
    Observations:
    There are only two tables in the URL

--- ExtractHTML.py
+++ ExtractHTML.py
@@ -1,24 +1,63 @@
 # get HTML code via URL
-import urllib.request  # Get HTML
-from bs4 import BeautifulSoup  # Navigate in HTML
+# Implementation references : thepythoncode.com/article/convert-html-tables-into-csv-files-in-python
+from bs4 import BeautifulSoup as bs  # Navigate in HTML
+import requests
 
 
-def UrltoHtml():
-    req = urllib.request.urlopen("https://wikipedia.com/wiki/Comparison_between_Esperanto_and_Ido")
+USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+# US english
+LANGUAGE = "en-US,en;q=0.5"
 
-    myhtml = req.read()
 
-    strhtml = myhtml.decode("utf8")
+def UrltoHtml(url):
+    """Get the soup using the HTML content of `url` """
 
-    req.close()
-    #print(strhtml)  #Print html of URL
-    return strhtml
+    # initialize a session
+    session = requests.Session()
+    # set the User-Agent as a regular browser
+    session.headers['User-Agent'] = USER_AGENT
+    # request for english content (optional)
+    session.headers['Accept-Language'] = LANGUAGE
+    session.headers['Content-Language'] = LANGUAGE
+    # make the request
+    html = session.get(url)
+    # return the soup
+    return bs(html.content, "html.parser")
 
-def GetTables(html_doc):
 
-    htmlparser = BeautifulSoup(html_doc, 'html.parser')
-    print("-" * 40)
-    tables = htmlparser.find_all('table')
-    #print(soup.prettify())
 
-    print(tables)
+def Get_All_Tables(soup):
+    """ Get all tables content of `url` """
+    tables = soup.find_all('table')
+    #print(tables)
+    return tables
+
+
+def Get_Table_Headers(table):
+    """ Get all headers for table content of `url` """
+    headers = []
+    for th in table.find("tr").find_all("th"):
+        headers.append(th.text.strip())
+    return headers
+
+
+def Get_Table_Rows(table):
+    """ Get all row for table content of `url` """
+    rows = []
+    for tr in table.find_all("tr")[1:]:
+        cells = []
+        # grab all td tags in this table row
+        tds = tr.find_all("td")
+        if len(tds) == 0:
+            # if no td tags, search for th tags
+            # can be found especially in wikipedia tables below the table
+            ths = tr.find_all("th")
+            for th in ths:
+                cells.append(th.text.strip())
+        else:
+            # use regular td tags
+            for td in tds:
+                cells.append(td.text.strip())
+        rows.append(cells)
+    return rows
+
--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import ExtractHTML
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
+import HTMLtoCSV
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    doc = ExtractHTML.UrltoHtml()
-    ExtractHTML.GetTables(doc)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    soup = ExtractHTML.UrltoHtml("https://wikipedia.com/wiki/Comparison_(grammar)")
+    tables = ExtractHTML.Get_All_Tables(soup)
+    print(f"[+] Found a total of {len(tables)} tables.")
 
+    # iterate over all tables
+    # iterate over all tables
+    for i, table in enumerate(tables, start=1):
+        # get the table headers
+        headers = ExtractHTML.Get_Table_Headers(table)
+        # get all the rows of the table
+        rows = ExtractHTML.Get_Table_Rows(table)
+        # save table as csv file
+        table_name = f"table-{i}"
+        print(f"[+] Saving {table_name}")
+        HTMLtoCSV.Save_As_Csv(table_name, headers, rows)
