7a69e896940c6873a95768698714f919787351c2
Johan LE BOURSICAUD <johan.lebours@gmail.com>
2020-11-03 15:38:05 +0100

    CreateCsvURL

--- HTMLtoCSV.py
+++ HTMLtoCSV.py
@@ -1,6 +1,25 @@
 import pandas as pd
+import ExtractHTML
 
-def Save_As_Csv(table_name, headers, rows):
+
+def save_As_Csv(table_name, headers, rows):
     """HTML to CSV for table content of `url` """
-
     pd.DataFrame(rows, columns=headers).to_csv(f"{table_name}.csv")
+
+
+# All tables of a URL
+def CreateCsv_URL(url):
+    soup = ExtractHTML.UrltoHtml(url)
+    tables = ExtractHTML.Get_All_Tables(soup)
+    print(f"[+] Found a total of {len(tables)} tables.")
+
+    # iterate over all tables
+    for i, table in enumerate(tables, start=1):
+        # get the table headers
+        headers = ExtractHTML.Get_Table_Headers(table)
+        # get all the rows of the table
+        rows = ExtractHTML.Get_Table_Rows(table)
+        # save table as csv file
+        table_name = f"table-{i}"
+        print(f"[+] Saving {table_name}")
+        save_As_Csv(table_name, headers, rows)
