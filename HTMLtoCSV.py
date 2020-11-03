fd489113a731a247af75e086ae373cb8ede3ac5d
Johan LE BOURSICAUD <johan.lebours@gmail.com>
2020-11-03 20:34:09 +0100

    creation of the output

--- HTMLtoCSV.py
+++ HTMLtoCSV.py
@@ -1,15 +1,26 @@
-import pandas as pd
+import pandas
 import ExtractHTML
-
+import os
 
 def save_As_Csv(table_name, headers, rows):
     """HTML to CSV for table content of `url` """
-    pd.DataFrame(rows, columns=headers).to_csv(f"{table_name}.csv")
+    pandas.DataFrame(rows, columns=headers).to_csv("./output", f"{table_name}.csv", index=False)
 
 
 # All tables of a URL
-def createCsv_URL(url):
-    soup = ExtractHTML.urlToHtml(url)
+def createCsv_URL(wurl, url):
+    if not (os.path.exists("./output")):
+        # define the name of the directory to be created
+        path = "./output"
+        # define the access rights
+        access_rights = 0o777
+        try:
+            os.mkdir(path, access_rights)
+        except OSError:
+            print("Creation of the directory %s failed" % path)
+        else:
+            print("Successfully created the directory %s" % path)
+    soup = ExtractHTML.urlToHtml(wurl)
     tables = ExtractHTML.get_All_Tables(soup)
     print(f"[+] Found a total of {len(tables)} tables.")
     # iterate over all tables
@@ -19,6 +30,6 @@ def createCsv_URL(url):
         # get all the rows of the table
         rows = ExtractHTML.get_Table_Rows(table)
         # save table as csv file
-        table_name = f"table-{i}"
+        table_name = f"{url}-{i}"
         print(f"[+] Saving {table_name}")
         save_As_Csv(table_name, headers, rows)
--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import HTMLtoCSV
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # Function General
-    HTMLtoCSV.createCsv_URL("https://en.wikipedia.org/wiki/Comparison_(grammar)")
+    HTMLtoCSV.createCsv_URL("https://en.wikipedia.org/wiki/Comparison_(grammar)", "Comparison_(grammar)")
 
-    with open("inputdata/wikiurls.txt", "r") as file:
-        for url in file:
-            wurl = "https://wikipedia.com/wiki/" + url
-            print(wurl)
-            HTMLtoCSV.createCsv_URL(wurl)
+    #with open("inputdata/wikiurls.txt", "r") as file:
+        #for url in file:
+            #wurl = "https://en.wikipedia.org/wiki/" + url
+            #print(wurl)
+            #HTMLtoCSV.createCsv_URL(wurl)
