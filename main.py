4a9d747fac8125791f4dd08fd9085313e39553dd
Johan LE BOURSICAUD <johan.lebours@gmail.com>
2020-11-03 16:29:48 +0100

    update main

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import ExtractHTML
 import HTMLtoCSV
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # Function General
-    #HTMLtoCSV.createCsv_URL("https://wikipedia.com/wiki/Comparison(grammar)")
+    HTMLtoCSV.createCsv_URL("https://en.wikipedia.org/wiki/Comparison_(grammar)")
 
     with open("inputdata/wikiurls.txt", "r") as file:
         for url in file:
             wurl = "https://wikipedia.com/wiki/" + url
+            print(wurl)
             HTMLtoCSV.createCsv_URL(wurl)
