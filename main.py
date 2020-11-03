7bdf45362e1df837aa2505c1e0cccbc14f6a408f
Léo Variéras <70764329+Lvarieras@users.noreply.github.com>
2020-11-03 16:06:38 +0100

    Algo url fini mais bugg lors du lancement

--- main.py
+++ main.py
@@ -5,21 +5,20 @@ import HTMLtoCSV
 
 
 if __name__ == '__main__':
-    fichier = open("inputdata/wikiurls.txt")
-    url = fichier.readline()
-    for i in url:
-        wurl = "BASE_WIKIPEDIA_URL" + i
-        #soup = ExtractHTML.UrltoHtml("https://wikipedia.com/wiki/Comparison_(grammar)")
-        soup = ExtractHTML.UrltoHtml(wurl)
-        tables = ExtractHTML.Get_All_Tables(soup)
-        print(f"[+] Found a total of {len(tables)} tables.")
-        # iterate over all tables
-        for i, table in enumerate(tables, start=1):
-            # get the table headers
-            headers = ExtractHTML.Get_Table_Headers(table)
-            # get all the rows of the table
-            rows = ExtractHTML.Get_Table_Rows(table)
-            # save table as csv file
-            table_name = f"table-{i}"
-            print(f"[+] Saving {table_name}")
-            HTMLtoCSV.Save_As_Csv(table_name, headers, rows)
+    with open("inputdata/wikiurls.txt" ,"r") as file :
+        for url in file :
+            wurl = "https://wikipedia.com/wiki/" + url
+            print(wurl)
+            soup = ExtractHTML.UrltoHtml(wurl)
+            tables = ExtractHTML.Get_All_Tables(soup)
+            print(f"[+] Found a total of {len(tables)} tables.")
+            #iterate over all tables
+            for i, table in enumerate(tables, start=1):
+                # get the table headers
+                headers = ExtractHTML.Get_Table_Headers(table)
+                # get all the rows of the table
+                rows = ExtractHTML.Get_Table_Rows(table)
+                # save table as csv file
+                table_name = f"table-{i}"
+                print(f"[+] Saving {table_name}")
+                HTMLtoCSV.Save_As_Csv(table_name, headers, rows)
