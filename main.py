189448ef07f23042e2404cc30af3491ad42439cd
Johan LE BOURSICAUD <johan.lebours@gmail.com>
2020-11-03 15:34:51 +0100

    CreateCsvURL

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import HTMLtoCSV
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # Function General
-    HTMLtoCSV.CreateCsvURL("https://wikipedia.com/wiki/Comparison(grammar)")
+    HTMLtoCSV.createCsvURL("https://wikipedia.com/wiki/Comparison(grammar)")
