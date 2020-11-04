71c4f428e6d7aac0ab232d36c9aa9aacce3f5f1f
KessalR <rabeaa.kessal@gmail.com>
2020-11-04 17:36:34 +0100

    test_sameFiles

--- main.py
+++ main.py
@@ -4,16 +4,12 @@ import HTMLtoCSV
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # Function General
-    #HTMLtoCSV.createCsv_URL("https://en.wikipedia.org/wiki/Comparison_(grammar)", "Comparison_(grammar)")
+    #Function General
+    HTMLtoCSV.createCsv_URL("https://en.wikipedia.org/wiki/Comparison_(grammar)", "Comparison_(grammar)")
 
-    #with open("inputdata/wikiurls.txt", "r") as file:
-        #for url in file:
-            #wurl = "https://en.wikipedia.org/wiki/" + url
-            #print(wurl)
-            #HTMLtoCSV.createCsv_URL(wurl, url)
+    with open("inputdata/wikiurls.txt", "r") as file:
+        for url in file:
+            wurl = "https://en.wikipedia.org/wiki/" + url
+            print(wurl)
+            HTMLtoCSV.createCsv_URL(wurl, url)
 
-    f1="output/Comparison_(grammar)-1.csv"
-    f2="output/Comparison_(grammar)-2.csv"
-
-    print(filecmp.cmp(f1,f2))
