260fa07d8a42e797c666892f35ad7a9e1a4c068e
KarlaRosas19 <49324882+KarlaRosas19@users.noreply.github.com>
2020-11-06 19:45:56 +0100

    Create result.text, this file contains the summary of tables found by url Structure url, number of tables

--- HTMLtoCSV.py
+++ HTMLtoCSV.py
@@ -7,3 +7,4 @@ def convert_csv(tables, name):
     for i, table in enumerate(tables, start=1):
         fullname = os.path.join(csvdir, csvname) + f"{i}.csv"
         table.to_csv(fullname, index=False)
+    return i
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import HTMLtoCSV
 import ExtractHTML
 
 if __name__ == '__main__':
+
     print("Reading url file...")
     # Two dimensional array string with urls and it's name for each one
     ExtractHTML.open_output()
@@ -11,7 +12,12 @@ if __name__ == '__main__':
     print(f"You will extract tables from {len(allUrls)} url(s)")
     print("Starting extraction...")
     nbInvalidUrl = 0
+    nbtaburl = []
+    nameurl = []
+    file = open("output/result.txt", "w")
+
     i = 0
+    k = 0
     for url, name in allUrls:
         i += 1
         print(f"Url {i} on {len(allUrls)}")
@@ -25,9 +31,30 @@ if __name__ == '__main__':
                     print("Exception type: ", exc.__class__)
                     print(f"A wikitable in the url {i} : {name} have a syntax problem, so it can't extract it")
                 else:
-                    HTMLtoCSV.convert_csv(dfs, name)
+                    k+=1
+                    a = HTMLtoCSV.convert_csv(dfs, name)
+                    #print("a= ", a)
+                    print("i", i)
+                    print("k", k)
+                    #print(name)
+
+                    nbtaburl.append(k)
+                    nbtaburl[k - 1] = a
+
+                    nameurl.append(k)
+                    nameurl[k - 1] = name
         else:
             nbInvalidUrl += 1
+    file.write("Results" + os.linesep)
+    for i in range(len(nbtaburl)):
+
+        a = nameurl[i]
+        b = str(nbtaburl[i])
+        file.write(a+" "+ b +os.linesep )
+
+
+    file.close()
     print(f"You extracted a total of {len(os.listdir('./output'))} table(s)")
+
     print(f"{nbInvalidUrl} url(s) was invalid")
     print("End of extraction, you can check the output directory :)")
